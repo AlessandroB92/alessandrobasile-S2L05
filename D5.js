937d36b0fb3d144ee9f6b6db73037358bb53cc17
AlessandroB92 <alessandrobasile1992@gmail.com>
2023-11-05 12:50:46 +0100

    fine progetto

--- D5.js
+++ D5.js
@@ -90,18 +90,29 @@ console.log(cars)
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
-const justTrims = []
+const senzaTrims = []
 cars.forEach(car => {
   if (car.trims.length > 0) {
-    justTrims.push(car.trims[0]);
+    senzaTrims.push(car.trims[0]);
   }
 });
-console.log(justTrims)
+console.log(senzaTrims)
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+
+for (let i = 0; i < cars.length; i++) {
+  const car = cars[i]
+  const firstLetter = car.color.charAt(0).toLowerCase()
+
+  if (firstLetter === 'b') {
+    console.log("Fizz");
+  } else {
+    console.log("Buzz");
+  }
+}
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
@@ -110,9 +121,40 @@ const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ]
 
+let i = 0;
+
+while (i < numericArray.length && numericArray[i] !== 32) {
+  console.log(numericArray[i]);
+  i++;
+}
+
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
     es. [f, b, e] --> [6, 2, 5]
 */
 const charactersArray = ['g', 'n', 'u', 'z', 'd']
+
+const positionsArray = [];
+
+for (let i = 0; i < charactersArray.length; i++) {
+  const char = charactersArray[i].toLowerCase()
+
+  switch (char) {
+    case 'g':
+      positionsArray.push(7);
+      break;
+    case 'n':
+      positionsArray.push(12);
+      break;
+    case 'u':
+      positionsArray.push(19);
+      break;
+    case 'z':
+      positionsArray.push(21);
+      break;
+    case 'd':
+      positionsArray.push(4);
+      break;
+  }}
+  console.log(charactersArray, positionsArray)
